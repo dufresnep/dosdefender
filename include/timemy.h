f4fb0d9f26f7ee09402c646e6c558720a06a345c
Paul Dufresne <dufresnep@zoho.com>
2025-03-26 01:59:48 -0400

    add missing timemy.c and timemy.h

---
+++ include/timemy.h
@@ -0,0 +1,14 @@
+#ifndef TIME_H
+#define TIME_H
+
+//#include <stdint.h>  it strangely make it like there is errors in defender.c/inlude Allegro
+#include <allegro.h> // Add this
+
+typedef uint32_t tick_t; // Define tick_t as uint32_t
+
+extern volatile tick_t ticks; // Declaration
+
+tick_t get_tick(void);
+uint32_t get_time(void);
+
+#endif
---
+++ src/timemy.c
@@ -0,0 +1,19 @@
+// src/time.c
+
+#include "timemy.h"
+#include <time.h>
+
+volatile tick_t ticks = 0; // Definition
+
+clock_t get_tick(void) { // Corrected return type
+    return clock();
+}
+
+uint32_t get_time(void) { // Corrected return type
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time (&rawtime);
+    timeinfo = localtime (&rawtime);
+    return (uint32_t)time(NULL); // Returns the number of seconds since the Epoch
+}
